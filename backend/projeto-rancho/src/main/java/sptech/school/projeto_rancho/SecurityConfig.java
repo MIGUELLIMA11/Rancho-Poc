6df0f4594ca7f509684421a84c65016a9bb436ee
MIGUELLIMA11 <miguellima@rancho.com>
2026-05-05 12:02:27 -0300

    fix: adiciona import @Autowired ausente no SecurityConfig

--- backend/projeto-rancho/src/main/java/sptech/school/projeto_rancho/SecurityConfig.java
+++ backend/projeto-rancho/src/main/java/sptech/school/projeto_rancho/SecurityConfig.java
@@ -1,6 +1,7 @@
 package sptech.school.projeto_rancho.config;
 
 import sptech.school.projeto_rancho.security.JwtFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
